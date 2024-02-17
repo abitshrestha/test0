bac7e12bfbcc5140a228a0054eade06a928b52eb
abitshrestha <abitshrestha2080@gmail.com>
2024-02-17 19:56:10 +0545

    jkk

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 connectDB();
 
 app.use(cors({
-    origin:['https://test0-phi.vercel.app'],
+    origin:['https://test0-phi.vercel.app/'],
     methods:['POST','GET'],
     credentials:true,
 }));
@@ -24,6 +24,9 @@ app.use(express.json());
 app.use('/images',express.static(path.join(__dirname,'uploads')));
 app.use('/',authRouter);
 app.use('/',uploadRouter)
+app.use('/check',(req,res)=>{
+    res.send('hello');
+})
 
 app.listen(port,()=>{
     console.log(`listening on http://localhost:${port}`);
